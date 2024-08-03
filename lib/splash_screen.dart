ac93555c3f1c88077f970b0d60d4f1d104955305
Ahmed_hamdy <118023886+SiNfuuL@users.noreply.github.com>
2024-08-03 13:47:56 +0300

    app styles done

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -20,6 +20,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return CustomBackground(
+      
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
