fdb0757128f24ac56bed27a74c4012a90a7390eb
Mohamed Ahmed <i.mohamedahhmed@gmail.com>
2024-08-06 16:15:49 -0700

    -edit featuers to features

--- lib/fatto_app.dart
+++ lib/fatto_app.dart
@@ -1,7 +1,7 @@
 import 'config/routes/app_router.dart';
 import 'config/routes/app_routes.dart';
 import 'exports.dart';
-import 'featuers/onBoarding/presintation/screens/first_onboarding_screen.dart';
+import 'features/onBoarding/presintation/screens/first_onboarding_screen.dart';
 
 class FattoApp extends StatelessWidget {
   final FattoRouter appRouter;
