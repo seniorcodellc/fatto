eabca9ee6080ef77574e6605d1f3cbf1e2ebb83a
axah710 <ahmed36085361@gmail.com>
2024-08-07 21:53:03 +0300

    feat: Implement onboarding feature and update various assets

--- lib/features/onBoarding/presentation/screens/onboarding_screen.dart
+++ lib/features/onBoarding/presentation/screens/onboarding_screen.dart
@@ -1,3 +1,4 @@
+import '../../../../config/routes/app_routes_helper.dart';
 import '../../../../core/utils/size_utils.dart';
 import '../../../../exports.dart';
 import '../../data/onboarding_model.dart';
@@ -119,11 +120,12 @@ class OnboardingScreen extends StatelessWidget {
 
                                   onPressed: () {
                                     // Navigate to the next onboarding screen
-                                    OnBoardingSlider.pageController.nextPage(
-                                      duration:
-                                          const Duration(milliseconds: 300),
-                                      curve: Curves.easeIn,
-                                    );
+                                    // OnBoardingSlider.pageController.nextPage(
+                                    //   duration:
+                                    //       const Duration(milliseconds: 300),
+                                    //   curve: Curves.easeIn,
+                                    // );
+                                    push(context:context, route: Routes.loginRoute);
                                   },
                                 ),
                               ),
